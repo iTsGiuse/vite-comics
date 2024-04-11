da391140a05d913ec8848b17d3807638213063a7
iTsGiuse <giuse.amato@icloud.com>
2024-04-11 16:21:59 +0200

    feat: add header

---
+++ src/components/header/AppHeader.vue
@@ -0,0 +1,58 @@
+<script>
+
+    export default {
+        name: 'AppHeader'
+    }
+
+</script>
+
+<template>
+
+    <header>
+        <div class="container d-flex justify-content-between align-items-center">
+            <div class="ms-box-image" >
+                <img class="img-fluid mt-2 mb-2" src="../../assets/img/dc-logo.png" alt="Logo">
+            </div>
+            <div class="me-5">
+                <ul>
+                    <li>CHARACTERS</li>
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP</li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+</template>
+
+<style scoepd lang="scss">
+
+    header{
+        height: 120px;
+        background-color: white;
+        
+        .container{
+            height: 100% !important;
+        }
+
+        li{
+            display: inline;
+            margin-left: 25px;
+            position: relative;
+
+            &:hover{
+                color: #0282f9;
+            }
+        }
+
+    }
+
+
+</style>
