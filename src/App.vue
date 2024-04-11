03dce6b0e048307909e0a99e57a1639ac6420343
iTsGiuse <giuse.amato@icloud.com>
2024-04-11 17:46:37 +0200

    feat: add "AppRegistrati"

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,17 @@
     import AppHeader from './components/header/AppHeader.vue';
     import AppDaFare from './components/main/AppDaFare.vue';
     import AppSectionBlue from './components/main/AppSectionBlue.vue';
-    
+    import AppRegistrati from './components/footer/AppRegistrati.vue';
+
+
     export default {
       components: {
         AppHeader,
         AppDaFare,
         AppSectionBlue,
+        AppRegistrati,
+
+        
       }
     }
 
@@ -19,11 +24,15 @@
   <AppHeader></AppHeader>
 
     <main>
-
       <AppDaFare></AppDaFare>
       <AppSectionBlue></AppSectionBlue>
-
     </main>
+
+    <footer>
+      <AppRegistrati></AppRegistrati>
+    </footer>
+
+
 
 </template>
 
---
+++ src/components/footer/AppRegistrati.vue
@@ -0,0 +1,80 @@
+<script>
+
+    export default {
+        name: 'AppRegistrati'
+    }
+
+</script>
+
+<template>
+
+    <section>
+        <div class="container d-flex justify-content-between align-items-center">
+            <div>
+                <button class="ms-btn">SIGN-UP NOW!</button>
+            </div>
+            <div class="d-flex align-items-center">
+                <div class="ms-box-text me-4">
+                    <span>FOLLOW US</span>
+                </div>
+                <div class="ms-box-image me-2">
+                    <img class="rounded" src="../../assets/img/footer-facebook.png" alt="Facebook">
+                </div>
+                <div class="ms-box-image me-2">
+                    <img class="rounded" src="../../assets/img/footer-twitter.png" alt="Twitter">
+                </div>
+                <div class="ms-box-image me-2">
+                    <img class="rounded" src="../../assets/img/footer-youtube.png" alt="YouTube">
+                </div>
+                <div class="ms-box-image me-2">
+                    <img class="rounded" src="../../assets/img/footer-pinterest.png" alt="Pinterest">
+                </div>
+                <div class="ms-box-image me-2">
+                    <img class="rounded" src="../../assets/img/footer-periscope.png" alt="Maps">
+                </div>
+            </div>
+        </div>
+    </section>
+
+</template>
+
+<style scoepd lang="scss">
+
+    section{
+        background-color: #303030;
+        height: 70px;
+
+        .container{
+            height: 100% !important;
+            width: 100% !important;
+
+            .ms-box-image{
+            width: 30px;
+            height: 30px;
+
+                img{
+                    width: 100% !important;
+
+                    &:hover{
+                        background-color: white;
+                    }
+                }
+
+            }
+
+            .ms-box-text{
+            color: #0282f9;
+            }
+
+            .ms-btn{
+                cursor: pointer;
+                padding: 10px;
+                color: white;
+                background-color: #303030;
+                border: 2px solid #0282f9;
+            }
+        }
+     
+    }
+
+</style>
